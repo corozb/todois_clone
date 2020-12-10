ed264a5b12b48c73a7939217f390682e8e0f7c0d
Cristian Orozco <cristianorozco@gmail.com>
2020-12-10 13:31:17 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Hello pues!</h1>
+      </header>
+    </div>
+  )
+}
